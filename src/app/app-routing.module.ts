fbad19db79851deee4a64fcb0e63fabe8f5af56d
Nicolas Endredi <nicolasendredi@gmail.com>
2022-06-07 14:53:02 +0200

    Routing ?

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { BsComponent } from './bs/bs.component';
 
 const routes: Routes = [
-  { path: 'toto', component: BsComponent }
+  { path: 'bs', component: BsComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { FooterComponent } from './footer/footer.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BsComponent } from './bs/bs.component';
+import { Comp1Component } from './comp1/comp1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    BsComponent
+    BsComponent,
+    Comp1Component
   ],
   imports: [
     BrowserModule,
