1a8047237f620c97eab2c40f54c79abc9a1e348a
Nicolas Endredi <nicolasendredi@gmail.com>
2022-06-08 10:46:32 +0200

    Cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,12 @@ import { BsComponent } from './bs/bs.component';
 import { Comp1Component } from './comp1/comp1.component';
 import { Comp1aComponent } from './comp1a/comp1a.component';
 import { Comp1bComponent } from './comp1b/comp1b.component';
-import { App1Component } from './app1/app1.component';
-import { App2Component } from './app2/app2.component';
-import { App3Component } from './app3/app3.component';
 import { LoginComponent } from './login/login.component';
 import { AppimgComponent } from './appimg/appimg.component';
 import { AppcarrComponent } from './appcarr/appcarr.component';
+import { App1Component } from './app1/app1.component';
+import { App2Component } from './app2/app2.component';
+import { App3Component } from './app3/app3.component';
 import { App4Component } from './app4/app4.component';
 import { App5Component } from './app5/app5.component';
 import { App6Component } from './app6/app6.component';
@@ -31,12 +31,12 @@ import { App7Component } from './app7/app7.component';
     Comp1Component,
     Comp1aComponent,
     Comp1bComponent,
-    App1Component,
-    App2Component,
-    App3Component,
     LoginComponent,
     AppimgComponent,
     AppcarrComponent,
+    App1Component,
+    App2Component,
+    App3Component,
     App4Component,
     App5Component,
     App6Component,
